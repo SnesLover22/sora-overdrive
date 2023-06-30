c73628566ca4208b9f50c206576091962dec7956
SnesLover22 <110874555+SnesLover22@users.noreply.github.com>
2023-06-29 20:10:31 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/static/tiw/',
+    prefix: '/sora-overdrive/static/tiw/',
     bare: 'https://baresw.starttiw.org',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/static/uv/uv.handler.js',
-    bundle: '/static/uv/uv.bundle.js',
-    config: '/static/uv/uv.config.js',
-    sw: '/static/uv/uv.sw.js',
+    handler: '/sora-overdrive/static/uv/uv.handler.js',
+    bundle: '/sora-overdrive/static/uv/uv.bundle.js',
+    config: '/sora-overdrive/static/uv/uv.config.js',
+    sw: '/sora-overdrive/static/uv/uv.sw.js',
 };
